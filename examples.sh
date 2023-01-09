c00dc060162586eaf73b80e0f206b6bc7ff299b5
Alec Savvy <alecjsavvy@gmail.com>
2023-01-08 19:42:59 -0700

    examples script

---
+++ examples.sh
@@ -0,0 +1,10 @@
+cd client
+
+cargo run --example users
+cargo run --example playlists
+cargo run --example tips
+cargo run --example tracks
+cargo run --example resolve
+# cargo run --example stream # doesn't work yet
+
+cd ../
